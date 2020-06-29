e1c5392e63af49aa89acf209a081b35a729399b9
Daniel Casey <dcasey@kingcounty.gov>
2020-06-29 10:17:07 -0700

    remove test files

--- test.bat
+++
@@ -1,4 +0,0 @@
-"C:\\Program Files\\R\\R-3.5.2\\bin\\Rscript.exe" --vanilla dist/script/R/run.R 1> log\\error1.log 2>&1
-"dist\\R-Portable\\App\\R-Portable\\bin\\Rscript.exe" --vanilla dist/script/R/run.R 1> log\\error2.log 2>&1
-"C:\\Program Files\\R\\R-4.0.0\\bin\\Rscript.exe" --vanilla dist/script/R/run.R 1> log\\error3.log 2>&1
-dist\\R-Portable\\App\\R-Portable\\bin\\Rscript.exe --vanilla dist/script/R/run.R 1> log\\error5.log 2>&1
