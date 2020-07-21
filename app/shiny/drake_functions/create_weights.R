446bc678818d3221d7e5917d7991b225ffd3cc52
Daniel Casey <dcasey@kingcounty.gov>
2020-07-21 11:45:12 -0700

    undo 1 ems order hard coding

--- app/shiny/drake_functions/create_weights.R
+++ app/shiny/drake_functions/create_weights.R
@@ -12,10 +12,10 @@ create_weights = function(ppe, hospital, ltcf, acrciq, chgs){
   w_ltcf = ltcf[agency %in% ppe[, agency], .(agency, type = 'ltcf', chg = cases * chgs[type == 'ltcf', chg], w2 = nbeds)]
   w_ltcf[is.na(chg), chg := 1] #place holder for facilities with no cases
   w_hosp = hospital$hosp_cases[name %in% ppe[, agency], .(agency = name, type = 'hospital', chg = covid * chgs[type == 'hospital', chg], w2 = covid)]
-  w_ph = unique(ppe[type %in% 'public health agency', .(agency, type, chg = chgs[type == 'public health agency', chg])])[, w2 := 1/.N]
+  w_ph = unique(ppe[type %in% 'public health agency', .(agency, type, chg = chgs[type == 'public health agency', chg, w2 = 1])])
   
-  stopifnot(length(unique(ppe[type == 'ems', agency]))<=1)
-  w_ems = unique(ppe[type == 'ems', .(agency, type, chg = chgs[type == 'ems', chg], w2 = 1)])
+  if(length(unique(ppe[type == 'ems', agency]))>1) warning('More than one EMS request detected')
+  w_ems = unique(ppe[type == 'ems', .(agency, type, chg = chgs[type == 'ems', chg, w2 = 1])])
   
   #create a combined weight matrix
   w = rbind(w_acrc, w_ltcf, w_hosp, w_ems, w_ph)
