99a8e00093933fed22569a412a947f8c4b61bc6d
Brown <alibrown@kingcounty.gov>
2020-09-25 16:05:27 -0700

    new input folder for all source files

--- app/shiny/helpers/build_folder_structure.R
+++ app/shiny/helpers/build_folder_structure.R
@@ -1,26 +1,24 @@
 #' Build the folder structure for a new round of stuff
-#' 
+#'
 #' @param date date- From the \code{Sys.Date()} type input. Specifies the date of the cycle. Usually the Friday when Logs sends a roundup to the state
 #' @param base_folder directory path- Location of the directory to create a cycle specific working folder
 #' @return path of the working directory. It also creates a few folders.
 build_folder_structure = function(date, base_folder){
-  
+
   stopifnot('Base working folder directory does not exist. Please create it or point to an existing folder' = dir.exists(base_folder))
 
   folder_name = paste0('distribute_',month(date),'-',mday(date),'-', year(date))
-  
+
   if(file.path(dirname(base_folder), folder_name) == base_folder){
     fold = file.path(dirname(base_folder), folder_name)
   }else{
     fold = file.path(base_folder, folder_name)
   }
-  
+
   dir.create(fold)
   dir.create(file.path(fold, 'inv'))
-  dir.create(file.path(fold, 'email_files'))
-  dir.create(file.path(fold, 'prior_week_files'))
-  dir.create(file.path(fold, 'web_download_files'))
-  
+  dir.create(file.path(fold, 'input_files'))
+
   return(fold)
 
 }
