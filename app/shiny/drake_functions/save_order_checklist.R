a780ad3a9d99dcde5e3ebfce0f3ad8d23782fdde
Hu <ahu@ph.lcl>
2021-03-23 13:35:50 -0700

    Update save_order_checklist.R
    Added address to orderlist file

--- app/shiny/drake_functions/save_order_checklist.R
+++ app/shiny/drake_functions/save_order_checklist.R
@@ -1,13 +1,13 @@
 
 save_order_checklist= function(pl){
   
-  pl = pl[,.(agency, order_ids, Region)]
+  pl = pl[,.(agency, order_ids, Region, Address)]
   
   # sort by region, agency
   setorder(pl, Region, agency)
   
   # tidy up column names
-  setnames(pl, c('Agency', 'Order IDs', 'Route'))
+  setnames(pl, c('Agency', 'Order IDs', 'Route', 'Address'))
   
   # add Delivered column
   pl$Delivered = ""
