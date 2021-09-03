147dab3108ad24e96907ec3d452bad66e6c2e50b
Hu <ahu@ph.lcl>
2021-09-03 14:34:57 -0700

    Update run_allocations_drake.R
    Updated regions for routing

--- app/shiny/helpers/run_allocations_drake.R
+++ app/shiny/helpers/run_allocations_drake.R
@@ -64,8 +64,8 @@ run_allocations_drake <- function(
   ltcf_categories = c('snf + alf', 'afh', 'supported living', 'alf', 'snf', 'ltcf')
 
   #routes by region
-  #regions = c('north_seattle_shoreline','bellevue','sw_king_county','east_king_county','renton','south_seattle_downtown','se_king_county', 'vashon')
-  regions = c('east_king_county','west_king_county','south_king_county', 'renton')
+  regions = c('north_seattle_shoreline','bellevue','sw_king_county','east_king_county','renton','south_seattle_downtown','se_king_county', 'vashon')
+  #regions = c('east_king_county','west_king_county','south_king_county')
 
   #Inputs! file paths
   template = file.path('./templates/template_order_1211.xlsx')
